51ed630c1834f2a13013a9df9dfa582dd9792ce6
AnaT1712 <anathernandeza1@gmail.com>
2025-05-10 14:15:25 -0600

    Create ejercicio6(if).c

---
+++ ejercicio6(if).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int a;
+   float aumen1;
+   float aument2;
+   float total;
+   
+   printf("dame una cantidad:");
+   scanf("%d", &a);
+   
+   if(a<1000)
+   {
+      aumen1= a*0.15;
+      total= a+aumen1;
+      
+      printf("el sueldo total será: %f\n", total);
+   }
+   
+   else 
+   
+   {
+      aumen1= a*0.12;
+      total= a+aumen1; 
+      printf("el sueldo total será: %f\n", total);
+   }
+   return 0;
+   
+}
