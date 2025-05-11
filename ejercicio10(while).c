8467a3d8616e563231a502290bc2f657ff822b6f
AnaT1712 <anathernandeza1@gmail.com>
2025-05-10 18:31:55 -0600

    Create ejercicio10(while).c

---
+++ ejercicio10(while).c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int i=0;
+    int pos=0;
+    int neg=0;
+    int nul=0;
+    
+    do
+    {
+        printf("dame un número:");
+        scanf("%d", &a);
+        
+        if (a==0)
+        {
+            nul=nul + 1;
+        }
+        else
+        {
+            if (a<0)
+            {
+                neg=neg + 1;
+            }
+            else
+            {
+                pos=pos + 1;
+            }
+        }
+        
+        i=i + 1;
+        
+    }
+    while(i<5);
+    
+    printf("la cantidad de números positivos es: %d\n", pos);
+    printf("la cantidad de números negativos es: %d\n", neg);
+    printf("la cantidad de números nulos es: %d\n", nul);
+    
+    return 0;
+}
