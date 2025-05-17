ef630674beeccaee3ca45516e759cd3861c76777
AnaT1712 <anathernandeza1@gmail.com>
2025-05-17 14:51:57 -0600

    Update ejercicio15 (arreglos).c

--- ejercicio15 (arreglos).c
+++ ejercicio15 (arreglos).c
@@ -7,9 +7,6 @@ int main()
     int pos=0;
     int neg=0;
     int nul=0;
-    int contnul=0;
-    int contneg=0;
-    int contpos=0;
     
     for(i=0; i<5; i++)
     { 
@@ -18,30 +15,29 @@ int main()
     }
     
     for(i=0; i<5; i++)
-    { 
-        if(list[i]==0)
+    {
+        if (list[i]==0)
         {
-            contnul= contnul+list[i];
-            contnul++;
+            nul=nul + 1;
         }
         else
         {
-            if(list[i]<0)
+            if (list[i]<0)
             {
-              contneg= contneg + list[i];
-              contneg++;
+                neg=neg + 1;
             }
-            else 
+            else
             {
-                contpos= contpos + list[i];
-                contpos++;
+                pos=pos + 1;
             }
         }
+        
     }
     
-    printf("La cantidad de numeros positivos es: %d\n", pos);
-    printf("la cantidad de numeros nulos es: %d\n",nul);
+    printf("la cantidad de numeros positivos es: %d\n", pos);
     printf("la cantidad de numeros negativos es: %d\n", neg);
+    printf("la cantidad de numeros nulos es: %d\n", nul);
     
-    return 0; 
+    return 0;
+    
 } 
